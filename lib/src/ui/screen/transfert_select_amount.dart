1a52b3af924b027ac2b272d9f47c58a201772b38
Tom JUMEL <Hh3XLtbfkF3dVN2>
2022-12-15 16:48:19 +0100

    UI improvement

--- lib/src/ui/screen/transfert_select_amount.dart
+++ lib/src/ui/screen/transfert_select_amount.dart
@@ -4,13 +4,9 @@ import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'package:local_auth/local_auth.dart';
 import 'package:local_auth_android/local_auth_android.dart';
 import 'package:local_auth_ios/local_auth_ios.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
-import 'package:sleek_circular_slider/sleek_circular_slider.dart';
-
 import 'package:payutc/compil.dart';
 import 'package:payutc/generated/l10n.dart';
 import 'package:payutc/src/models/transfert.dart';
@@ -19,6 +15,9 @@ import 'package:payutc/src/services/app.dart';
 import 'package:payutc/src/services/random_sentence.dart';
 import 'package:payutc/src/services/search_user_manager.dart';
 import 'package:payutc/src/ui/screen/select_amount.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+
 import '../style/color.dart';
 
 class SelectTransfertAmountScreen extends StatefulWidget {
@@ -137,10 +136,21 @@ class _SelectTransfertAmountScreenState
                             );
                           },
                           child: Center(
-                            child: Text(
-                              "${(amount).toStringAsFixed(2)}€",
-                              style: const TextStyle(fontSize: 25),
-                              textAlign: TextAlign.center,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  "${(amount).toStringAsFixed(2)}€",
+                                  style: const TextStyle(fontSize: 25),
+                                  textAlign: TextAlign.center,
+                                ),
+                                const Text(
+                                  "Changer",
+                                  style: TextStyle(
+                                      fontSize: 13, color: AppColors.orange),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ],
                             ),
                           ),
                         ),
