4f28c8696760d7092be8ee87bfee91edef940f79
Armand Ribault <ribaultarmand@gmail.com>
2023-02-04 13:59:56 +0100

    remove unused SizedBox()

--- lib/src/ui/screen/home.dart
+++ lib/src/ui/screen/home.dart
@@ -159,9 +159,6 @@ class _HomePageState extends State<HomePage>
                           ),
                         ),
                       ),
-                      const SizedBox(
-                        height: 15,
-                      ),
                     ],
                   ),
                 ),
